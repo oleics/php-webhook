2848cf5a7330ab4debf5d33625d35ac4636accf6
Mikhail Kuklin <mikhail@clevertech.biz>
2012-07-30 19:16:02 +0400

    my codestyle

--- hook.php
+++ hook.php
@@ -2,14 +2,16 @@
 
 require_once('inc.php');
 header('Content-Type: text/plain');
-if(file_exists('key.php')) {
-  $key = require('key.php');
-  if($key['current'] === $_SERVER['QUERY_STRING']) {
-    $pwd = getcwd();
-    chdir(dirname($pwd));
-    _exec('pwd');
-    _exec('git pull');
-  }
+if (file_exists('key.php'))
+{
+    $key = require('key.php');
+    if ($key['current'] === $_SERVER['QUERY_STRING'])
+    {
+        $pwd = getcwd();
+        chdir(dirname($pwd));
+        _exec('pwd');
+        _exec('git pull');
+    }
 }
 
 _log(var_export($_POST, true));
--- inc.php
+++ inc.php
@@ -2,14 +2,16 @@
 
 #error_reporting(0);
 
-function _exec($cmd) {
-  $out = "$ ".$cmd."\n";
-  $out .= shell_exec($cmd.' 2>&1');
-  _log($out);
+function _exec($cmd)
+{
+    $out = "$ " . $cmd . "\n";
+    $out .= shell_exec($cmd . ' 2>&1');
+    _log($out);
 }
 
-function _log($str) {
-  $out = "\n".date('r')." ".$str;
-  echo $out;
-  error_log($out, 3, '.webhook.log');
+function _log($str)
+{
+    $out = "\n" . date('r') . " " . $str;
+    echo $out;
+    error_log($out, 3, '.webhook.log');
 }
